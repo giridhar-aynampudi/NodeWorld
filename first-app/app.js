b051bd6aa8554386441264b58be9f8c42fee1533
Giridhar <giridharvarma.aynampudi@gmail.com>
2020-09-11 12:02:31 +1000

    file modules
    imp: use always to async

--- first-app/app.js
+++ first-app/app.js
@@ -1,6 +1,8 @@
 const log = require("./logger");
 const path = require("path");
 const os = require("os");
+const fs = require("fs");
+
 function hello(name) {
   console.log("hello " + name);
 }
@@ -17,3 +19,11 @@ var totalMemory = os.totalmem();
 var freeMemory = os.freemem();
 console.log(`Total Memory: ${totalMemory}`);
 console.log(`Free Memory: ${freeMemory}`);
+
+const file = fs.readdirSync("./");
+console.log(file);
+
+fs.readdir("$", function (err, files) {
+  if (err) console.log("Error", err);
+  else console.log("result", files);
+});
