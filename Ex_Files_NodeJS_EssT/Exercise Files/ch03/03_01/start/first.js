027f74564dc9f95e949b896067682cd2b96765d3
Giridhar <giridharvarma.aynampudi@gmail.com>
2020-09-11 10:31:26 +1000

    first commit

---
+++ Ex_Files_NodeJS_EssT/Exercise Files/ch03/03_01/start/first.js
@@ -0,0 +1,8 @@
+let men = "👨";
+let women = "👩";
+console.log(men < women ? true : false);
+// output: true, women are greater/stronger than men
+
+console.log("Hello this is chitti");
+
+global.console.log("asdasdasdasd");
