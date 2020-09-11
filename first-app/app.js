b1a277cabd0223dc885f24d006e12260351c605f
Giridhar <giridharvarma.aynampudi@gmail.com>
2020-09-11 11:52:16 +1000

    path and os modules

--- first-app/app.js
+++ first-app/app.js
@@ -1,5 +1,6 @@
 const log = require("./logger");
-
+const path = require("path");
+const os = require("os");
 function hello(name) {
   console.log("hello " + name);
 }
@@ -8,3 +9,11 @@ hello("giridhar");
 
 // console.log(window);
 log.log("yo");
+
+var pathObj = path.parse(__filename);
+console.log(pathObj);
+
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+console.log(`Total Memory: ${totalMemory}`);
+console.log(`Free Memory: ${freeMemory}`);
