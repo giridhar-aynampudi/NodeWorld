d15b7557a72c74461dcad04adecee2c06cb33c80
Giridhar <giridharvarma.aynampudi@gmail.com>
2020-09-21 14:58:42 +1000

    Joi package and post method with validation

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
-
+const Joi = require("joi");
 const app = express();
+
+app.use(express.json());
 
 //methods corresponds to HTTP methods
 // app.get()
@@ -19,7 +21,25 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/courses", (req, res) => {
-  res.send([1, 2, 3]);
+  res.send(courses);
+});
+
+app.post("/api/courses", (req, res) => {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+  const result = Joi.validate(req.body, schema);
+  //   console.log(result);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
 });
 
 app.get("/api/courses/:id", (req, res) => {
