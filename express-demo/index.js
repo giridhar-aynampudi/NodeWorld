6aef7fea5ba674ba8fd3ce7dcd9df5a64fd86757
Giridhar <giridharvarma.aynampudi@gmail.com>
2020-09-21 14:25:40 +1000

    get method, route and params

--- express-demo/index.js
+++ express-demo/index.js
@@ -8,12 +8,24 @@ const app = express();
 // app.put()
 // app.delete()
 
+const courses = [
+  { id: 1, name: "c1" },
+  { id: 2, name: "c2" },
+  { id: 3, name: "c3" },
+];
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
 app.get("/api/courses", (req, res) => {
   res.send([1, 2, 3]);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("the course not found"); // 404
+  res.send(course);
 });
 
 //port
