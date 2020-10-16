ce3360c4cf595ad147a1462d063f04640a1b749e
Giridhar <giridharvarma.aynampudi@gmail.com>
2020-10-16 12:21:19 +1100

    bycrpt and password hashing

--- simpleLogin/server.js
+++ simpleLogin/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const bcrypt = require("bcrypt");
 // import './views/index';
 app.set("view-engine", 'ejs')
 app.use(express.urlencoded({extended: false}))
+
+const users = [];
 
 app.get('/', (req,res) => {
     res.render('index.ejs',{name: "Giridhar"});
@@ -16,11 +19,24 @@ app.get('/register', (req,res) =>{
     res.render("register.ejs")
 });
 
-app.post('/register',(req,res) => {
-    console.log("register", req.body);
+app.post('/register',async (req,res) => {
+    console.log("register", req.body.email);
+    try {
+         const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push ({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch {
+        res.redirect('/register')
+    }
+    console.log(users)
 });
 
 app.post("/login", (req,res) => {
-console.log("login", req.body.email)
+// console.log("login", req.body.email)
 })
 app.listen(3000);
